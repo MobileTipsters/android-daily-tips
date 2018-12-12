e4ae6d2c2f6a9cbe5fd15661bb4a732423ef509e
Furkan Aşkın <askinn.furkan@gmail.com>
2018-12-12 23:43:52 +0300

    Repeat Function Fix

--- codes/RepeatFunction.kt
+++ codes/RepeatFunction.kt
@@ -1,7 +1,8 @@
-inline fun repeat(times: Int, action: (Int) -> Unit)
+/*inline fun repeat(times: Int, action: (Int) -> Unit)
 Executes the given function action specified number of times.
 
 A zero-based index of current iteration is passed as a parameter to action.
+*/
 
 // Say awesome ten times
 repeat(10) {
