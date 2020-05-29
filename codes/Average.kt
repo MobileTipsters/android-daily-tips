a7d28981bd2a923e2b20ebf450a164b306874d8c
faskN <seeingisknow@gmail.com>
2020-05-29 12:57:47 +0300

    [Fixes #82] Naming issue

--- codes/Average.kt
+++ codes/Average.kt
@@ -1,7 +1,7 @@
 val list = arrayListOf<Double>(2.0, 3.0, 7.0)
-var average = 0
+var sum = 0
 list.forEach{
-    average += it.toInt()
+    sum += it.toInt()
 }
-println(average/list.size) // 4
+println(sum/list.size) // 4
 println(list.average().toInt()) // 4
